4d9b52b62d7fc0f29612de565d145b2366e2b4bf
bkorcan <bkorcan@gmail.com>
2022-06-21 05:38:17 +0300

    2

--- components/map.js
+++ components/map.js
@@ -3,7 +3,7 @@ import {
 } from "react";
 import {
   GoogleMap, useLoadScript, Marker, MarkerClusterer, OverlayView
-} from "@react-google-maps/api";"@react-google-maps/api";
+} from "@react-google-maps/api"; "@react-google-maps/api";
 
 function MapHome() {
   const { isLoaded } = useLoadScript({
@@ -17,11 +17,33 @@ function MapHome() {
 function Map() {
   const center = useMemo(() => ({ lat: 36.2008559, lng: 29.6453682 }), []);
 
+  const markerData = [{ position: { lat: 36.2008559, lng: 29.6453682 }, price: '100' }, { position: { lat: 36.205, lng: 29.6353682 }, price: '200' }];
   return (
     <GoogleMap zoom={14} center={center} mapContainerClassName="map-container">
-      <Marker position={center} />
+      {/* Clustering Marker */}
+      <MarkerClusterer>
+        {() =>
+          markerData.map(villas =>
+            <Marker position={{ lat: villas.position.lat, lng: villas.position.lng }}
+              label={villas.price}
+              icon={{
+                path:
+                  "M-9,-5 l18,0 l0,9 l-18,0 l0,-9",
+                fillColor: "white",
+                fillOpacity: 1,
+                scale: 3,
+                strokeColor: "red",
+                strokeWeight: 1.5,
+              }}
+
+            />
+          )
+        }
+      </MarkerClusterer>
+      {/* End Of Clustering Marker */}
+
     </GoogleMap>
   );
 }
 
-export {MapHome}
+export { MapHome }
